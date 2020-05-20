ce1e87de870426a19092c1adbafd35c0c59dd66f
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-05-20 19:56:28 -0300

    Programa finalizado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,11 +10,12 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		
 		double largura, altura;
+		int option;
 		
 		System.out.print("Digite a largura do retângulo: ");
 		largura = sc.nextDouble();
 		
-		while (largura < 0) {
+		while (largura <= 0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			largura = sc.nextDouble();
 		}
@@ -22,11 +23,90 @@ public class Programa {
 		
 		System.out.print("Digite a altura do retângulo: ");
 		altura = sc.nextDouble();
+		System.out.println();
 		
-		while (altura < 0) {
+		while (altura <= 0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			altura = sc.nextDouble();
+			System.out.println();
 		}
+
+		System.out.println("MENU:");
+		System.out.println();
+		System.out.println("1-Mostrar area");
+		System.out.println("2-Mostrar perimetro");
+		System.out.println("3-Sair");
+		System.out.print("Digite uma opcao: ");
+		option = sc.nextInt();
+		
+		while(option != 1 && option != 2 && option != 3) {
+			System.out.println();
+			System.out.println("OPÇÃO INVALIDA");
+			System.out.println();
+			System.out.println("MENU:");
+			System.out.println();
+			System.out.println("1-Mostrar area");
+			System.out.println("2-Mostrar perimetro");
+			System.out.println("3-Sair");
+			System.out.print("Digite uma opcao: ");
+			option = sc.nextInt();
+		}
+		while(option == 1 || option == 2) {
+		if (option == 1) {
+			System.out.println();
+			System.out.println("AREA = " + (altura*largura));
+			System.out.println();
+			System.out.println("MENU:");
+			System.out.println();
+			System.out.println("1-Mostrar area");
+			System.out.println("2-Mostrar perimetro");
+			System.out.println("3-Sair");
+			System.out.print("Digite uma opcao: ");
+			option = sc.nextInt();
+			
+			while(option != 1 && option != 2 && option != 3) {
+				System.out.println();
+				System.out.println("OPÇÃO INVALIDA");
+				System.out.println();
+				System.out.println("MENU:");
+				System.out.println();
+				System.out.println("1-Mostrar area");
+				System.out.println("2-Mostrar perimetro");
+				System.out.println("3-Sair");
+				System.out.print("Digite uma opcao: ");
+				option = sc.nextInt();
+			}
+		}
+		else if (option == 2) {
+			System.out.println();
+			System.out.println("PERÍMETRO = " + (largura+largura+altura+altura));
+
+			System.out.println();
+			System.out.println("MENU:");
+			System.out.println();
+			System.out.println("1-Mostrar area");
+			System.out.println("2-Mostrar perimetro");
+			System.out.println("3-Sair");
+			System.out.print("Digite uma opcao: ");
+			option = sc.nextInt();
+			
+			while(option != 1 && option != 2 && option != 3) {
+				System.out.println();
+				System.out.println("OPÇÃO INVALIDA");
+				System.out.println();
+				System.out.println("MENU:");
+				System.out.println();
+				System.out.println("1-Mostrar area");
+				System.out.println("2-Mostrar perimetro");
+				System.out.println("3-Sair");
+				System.out.print("Digite uma opcao: ");
+				option = sc.nextInt();
+			}
+			}
+		
+		}
+		System.out.println();
+		System.out.println("FIM DO PROGRAMA!");
 		
 		sc.close();
 	}
