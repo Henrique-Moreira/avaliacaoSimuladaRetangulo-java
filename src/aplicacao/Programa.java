18d5252681c42433ef795d57f8421d6f2bfee917
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-05-20 19:27:25 -0300

    Leitura da largura com validacao

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,27 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura, altura;
+		
+		System.out.print("Digite a largura do retângulo: ");
+		largura = sc.nextDouble();
+		
+		while (largura < 0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			largura = sc.nextDouble();
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
